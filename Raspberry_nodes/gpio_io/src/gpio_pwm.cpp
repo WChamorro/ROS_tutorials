1e2614c7fbd1938ae7feca1335559ce3f255ce52
WChamorro <wchamorro@iri.edu.ec>
2023-10-09 14:40:36 -0500

    fixing pwm

--- Raspberry_nodes/gpio_io/src/gpio_pwm.cpp
+++ Raspberry_nodes/gpio_io/src/gpio_pwm.cpp
@@ -1,26 +1,33 @@
 #include <ros/ros.h>
 #include <wiringPi.h>
-#include <softPwm.h>
+#include <softPwm.h>	
 #include <std_msgs/Int32.h>
 #include <iostream>
 #include <sstream>
-int range = 255;
+int range = 100;
+int pin = 12;
+
+
 void pwm_callback(const std_msgs::Int32::ConstPtr & msg){
 	int width = msg->data;
 	if(width>range){
 		width=255;
 	}
-	pwmWrite(3, width);
+	softPwmWrite(pin,width);
+	std::cout<<"pwm set in "<<width<<"\n";
 }
+
 
 int main (int argc, char **argv)
 {
 	ros::init(argc, argv,"gpio_pwm_subscriber");
 	ros::NodeHandle nh;
 	ros::Subscriber pwm_subscriber = nh.subscribe("/pwm",10,pwm_callback);
+
 	wiringPiSetupGpio();
-	pinMode(3, OUTPUT);
-	pwmSetRange(range);
+    	ROS_INFO("GPIO has been set as OUTPUT.");
+   	 pinMode(pin,OUTPUT);
+   	 softPwmCreate(pin,0,range);
 	ros::spin();
 	return 0;
 }
